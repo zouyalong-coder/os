8178b28fa72b4c80406134b00e0cca9f573b654b
zouyalong <zouyalong@bytedance.com>
2023-02-11 20:54:37 +0800

    支持集成测试,并分离出 lib.rs 复用

---
+++ kernel/src/lib.rs
@@ -0,0 +1,58 @@
+// 使用 lib 分离，使得关于 no_std 部分的代码可以在不同的 crate 中复用，比如集成测试中的代码。
+#![no_std]
+#![cfg_attr(test, no_main)] // 在 test 模式下，采用 no_main
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)] // 指定测试框架
+// 测试函数生成的 main 函数改为 test_main
+#![reexport_test_harness_main = "test_main"] // 测试框架入口设置为 test_main
+
+use core::panic::PanicInfo;
+
+mod qemu;
+pub mod serial;
+pub mod vga_buffer;
+
+// cargo test 的入口函数
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    qemu::exit_qemu(qemu::QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        // 获取函数名
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    qemu::exit_qemu(qemu::QemuExitCode::Success);
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,45 +3,30 @@
 // 禁用所有 Rust 层级的入口点
 // 自定义测试框架会生成 main 函数用于测试，但由于我们使用了 no_main，是入口变成了 _start
 #![feature(custom_test_frameworks)] // no_std 禁用了默认的测试框架，需要自定义
-#![test_runner(crate::test::test_runner)] // 指定测试框架的入口
+#![test_runner(kernel::test_runner)] // 指定测试框架的入口
 // 使得测试生成的 main 函数改为 test_main
 #![reexport_test_harness_main = "test_main"] // 重新导出测试框架的入口
 
-mod qemu;
-pub mod serial;
-mod test;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
+use kernel::println;
 
-static HELLO: &[u8] = b"Hello World!";
-
+#[cfg(test)]
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
     // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
     // 默认命名为 `_start`
-
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_string("Hello again!\n");
-    {
-        writeln!(
-            vga_buffer::WRITER.lock(),
-            ", some numbers: {} {}",
-            42,
-            1.337
-        );
-    }
-
-    #[cfg(test)]
+    println!("Hello World{}", "!");
     test_main();
+
+    loop {}
+}
+
+#[cfg(not(test))]
+#[no_mangle] // 不重整函数名
+pub extern "C" fn _start() -> ! {
+    // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
+    // 默认命名为 `_start`
+    println!("Hello World{}", "!");
 
     loop {}
 }
@@ -57,22 +42,18 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    qemu::exit_qemu(qemu::QemuExitCode::Failed);
-    loop {}
+    kernel::test_panic_handler(info)
 }
 
 #[test_case]
 fn trivial_assertion() {
     // serial_println!("trivial assertion... ");
-    assert_eq!(1, 0);
+    assert_eq!(1, 1);
     // serial_println!("[ok]");
 }
 
 #[cfg(test)]
 mod tests {
-    use super::serial_println;
 
     #[test_case]
     fn it_works() {
